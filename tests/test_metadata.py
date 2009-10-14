af3c668e854582d5a9beb2f8bf97dec4b9c04f88
Roland Hedberg <roland.hedberg@adm.umu.se>
2009-10-14 05:48:29 +0200

    More tests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,16 +1,35 @@
 from saml2 import metadata
 
-SWAMI_METADATA = "urn-mace-swami.se-swamid-test-1.0-metadata.xml"
+SWAMI_METADATA = "tests/urn-mace-swami.se-swamid-test-1.0-metadata.xml"
+INCOMMON_METADATA = "tests/InCommon-metadata.xml"
 
 def test_swami_1():
-    idps = metadata.import_metadata(open(SWAMI_METADATA).read())
-    print len(idps)
-    assert len(idps) == 1
-    print idps.keys()
-    assert idps.keys() == ['https://idp.umu.se/saml2/idp/SSOService.php']
+    md = metadata.MetaData()
+    md.import_metadata(open(SWAMI_METADATA).read())
+    print len(md)
+    assert len(md) == 1
+    print md.keys()
+    assert md.keys() == ['https://idp.umu.se/saml2/idp/metadata.php']
+    idp_sso = md.single_sign_on_services('https://idp.umu.se/saml2/idp/metadata.php')
+    assert len(idp_sso) == 1
+    assert idp_sso == ['https://idp.umu.se/saml2/idp/SSOService.php']
+    ssocerts =  md.certs('https://idp.umu.se/saml2/idp/SSOService.php')
+    print ssocerts
+    assert len(ssocerts) == 1
     
-    (odnl,certs) = idps['https://idp.umu.se/saml2/idp/SSOService.php']
-    
-    c_mngr = metadata.load_certs_to_manager(certs)
-    assert c_mngr
-    
+def test_incommon_1():
+    md = metadata.MetaData()
+    md.import_metadata(open(INCOMMON_METADATA).read())
+    print len(md)
+    assert len(md) == 35
+    print md.keys()
+    idp_sso = md.single_sign_on_services('urn:mace:incommon:uiuc.edu')
+    assert idp_sso == []
+    idp_sso = md.single_sign_on_services('urn:mace:incommon:alaska.edu')
+    assert len(idp_sso) == 1
+    print idp_sso
+    assert idp_sso == ['https://idp.alaska.edu/idp/profile/SAML2/Redirect/SSO']
+    redirect_idps = [eid for eid in md.keys() if len( md.single_sign_on_services(eid))]
+    print redirect_idps
+    assert len(redirect_idps) == 8 # !!!!????
+        
