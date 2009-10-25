20b1f46c08eb38e6048dd7c31d416c8e4208d795
Roland Hedberg <roland.hedberg@adm.umu.se>
2009-10-25 21:19:02 +0100

    Tests of generalize making metadata

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,4 +1,5 @@
 from saml2 import metadata
+from saml2 import md
 
 SWAMI_METADATA = "tests/urn-mace-swami.se-swamid-test-1.0-metadata.xml"
 INCOMMON_METADATA = "tests/InCommon-metadata.xml"
@@ -89,12 +90,60 @@ def test_construct_contact():
     assert c.sur_name.text == "Hedberg"
     assert c.email_address[0].text == "roland@catalogix.se"    
     assert _eq(c.keyswv(), ["given_name","sur_name","email_address"])
+
+def test_construct_organisation_name():
+    o = md.Organization()
+    metadata._make_vals(o, {"text":"Exempel AB", "lang":"se"}, 
+            "organization_name", md.OrganizationName)
+    assert str(o) == """<?xml version='1.0' encoding='UTF-8'?>
+<ns0:Organization xmlns:ns0="urn:oasis:names:tc:SAML:2.0:metadata"><ns0:OrganizationName ns1:lang="se" xmlns:ns1="http:#www.w3.org/XML/1998/namespace">Exempel AB</ns0:OrganizationName></ns0:Organization>"""
+            
+def test_construct_organisation():
+    c = metadata.make_xyz( md.Organization, {
+            "organization_name": ["Example Co.",
+                    {"text":"Exempel AB", "lang":"se"}],
+            "organization_url": "http://www.example.com/"
+        })
+        
+    assert _eq(c.keyswv(), ["organization_name","organization_url"])
+    assert len(c.organization_name) == 2
+    org_names = [on.text for on in c.organization_name]
+    assert _eq(org_names,["Exempel AB","Example Co."])
+    assert len(c.organization_url) == 1
     
-#def test_construct_metadata():
-#    ed = metadata.make_entity_description(
-#        {"organisation":{
-#            "name":"Catalogix", "url": "http://www.catalogix.se/"},
-#         "entity_id": "urn:mace:catalogix.se:sp1",   
-#        })
-#    print ed
-#    assert False
+def test_construct_entity_descr_1():
+    ed = metadata.make_xyz(md.EntityDescriptor,
+        {"organization": {
+            "organization_name":"Catalogix", 
+            "organization_url": "http://www.catalogix.se/"},
+         "entity_id": "urn:mace:catalogix.se:sp1",   
+        })
+
+    assert ed.entity_id == "urn:mace:catalogix.se:sp1"
+    org = ed.organization
+    assert _eq(org.keyswv(), ["organization_name","organization_url"])
+    assert len(org.organization_name) == 1
+    assert org.organization_name[0].text == "Catalogix"
+    assert org.organization_url[0].text == "http://www.catalogix.se/"
+
+def test_construct_entity_descr_2():
+    ed = metadata.make_xyz(md.EntityDescriptor,
+        {"organization": {
+            "organization_name":"Catalogix", 
+            "organization_url": "http://www.catalogix.se/"},
+         "entity_id": "urn:mace:catalogix.se:sp1",
+         "contact_person": {
+            "given_name":"Roland",
+            "sur_name": "Hedberg",
+            "email_address": "roland@catalogix.se",
+            }   
+        })
+
+    assert _eq(ed.keyswv(), ["entity_id", "contact_person", "organization"])
+    assert ed.entity_id == "urn:mace:catalogix.se:sp1"
+    org = ed.organization
+    assert _eq(org.keyswv(), ["organization_name", "organization_url"])
+    assert len(org.organization_name) == 1
+    assert org.organization_name[0].text == "Catalogix"
+    assert org.organization_url[0].text == "http://www.catalogix.se/"
+    
