7e417cc7b08b2faa4920e2bce00500bd7c2848aa
Roland Hedberg <roland.hedberg@adm.umu.se>
2009-10-30 15:52:45 +0100

    removed old utils file

--- src/saml2/saml.py
+++ src/saml2/saml.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #
 # Copyright (C) 2007 SIOS Technology, Inc.
+# Copyright (C) 2009 Umeå University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
