1479d247ee8ecd32ab1954cfc50ac214ab6f121e
Roland Hedberg <roland.hedberg@adm.umu.se>
2009-11-02 10:03:21 +0100

    Saml2 in Repoze

--- setup.py
+++ setup.py
@@ -31,7 +31,9 @@ python-saml2 is a library for SAML Version 2.
     author_email='roland.hedberg@adm.umu.se',
     license='Apache 2.0',
     url='http://code.google.com/p/python-saml2/',
-    packages=['saml2', 'xmldsig', 'xmlenc'],
+    packages=['saml2', 'xmldsig', 'xmlenc', 'saml2_repoze', 
+                'saml2_repoze.plugins'],
     package_dir = {'saml2':'src/saml2', 'xmldsig':'src/xmldsig',
-                    'xmlenc': 'src/xmlenc'}
+                    'xmlenc': 'src/xmlenc', 
+                    'saml2_repoze': 'src/saml2_repoze'}
 )
