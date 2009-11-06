e8a2abf8cf173de6a8c9caec469f8d0ca73a34cc
Roland Hedberg <roland.hedberg@adm.umu.se>
2009-11-06 19:41:36 +0100

    Improved usability

--- src/saml2/time_util.py
+++ src/saml2/time_util.py
@@ -162,14 +162,16 @@ def add_duration(tid, duration):
 
 # ---------------------------------------------------------------------------
 
-def in_a_while(*args):
+def in_a_while(days=0, seconds=0, microseconds=0, milliseconds=0,
+                minutes=0, hours=0, weeks=0):
     """
     format of timedelta:
         timedelta([days[, seconds[, microseconds[, milliseconds[,
                     minutes[, hours[, weeks]]]]]]])
     """
     now = datetime.now()
-    t = timedelta(*args)
+    t = timedelta(*[days,seconds,microseconds,milliseconds,minutes,
+                    hours,weeks])
     soon = now + t
     return soon.strftime(TIME_FORMAT)
 
