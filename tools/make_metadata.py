a72044c2367aa11ad9e19bee2d397a4c7f5bea8f
Roland Hedberg <roland.hedberg@adm.umu.se>
2009-11-20 14:28:08 +0100

    Enable requested attributes

--- tools/make_metadata.py
+++ tools/make_metadata.py
@@ -3,10 +3,11 @@ import os
 from saml2 import utils, md, samlp, BINDING_HTTP_POST, BINDING_HTTP_REDIRECT
 from saml2 import BINDING_SOAP
 from saml2.time_util import in_a_while
+from saml2.utils import parse_attribute_map
+from saml2.saml import NAME_FORMAT_URI
 
-
-def do_sp_sso_descriptor(sp, cert):
-    return {
+def do_sp_sso_descriptor(sp, cert, backward_map):
+    desc = {
         "protocol_support_enumeration": samlp.NAMESPACE,
         "want_assertions_signed": True,
         "authn_requests_signed": False,
@@ -23,6 +24,35 @@ def do_sp_sso_descriptor(sp, cert):
                 }
             },
         }
+        
+    requested_attribute = []
+    if "required_attributes" in sp:
+        for attr in sp["required_attributes"]:
+            requested_attribute.append({
+                "is_required": "true",
+                "friendly_name": attr,
+                "name_format": NAME_FORMAT_URI,
+                "name": backward_map[attr]
+            })
+        
+    if "optional_attributes" in sp:
+        for attr in sp["optional_attributes"]:
+            requested_attribute.append({
+                "friendly_name": attr,
+                "name_format": NAME_FORMAT_URI,
+                "name": backward_map[attr]
+            })
+    
+    if requested_attribute:
+        desc["attribute_consuming_service"] = {
+            "requested_attribute": requested_attribute,
+            "service_name": {
+                "lang":"en",
+                "text":sp["my_name"],
+            }
+        }
+        
+    return desc
 
 def do_idp_sso_descriptor(idp, cert):
     return {
@@ -60,6 +90,10 @@ def do_aa_descriptor(aa, cert):
 def entity_descriptor(confd):
     mycert = "".join(open(confd["cert_file"]).readlines()[1:-1])
     
+    backward_map = {}
+    if "attribute_maps" in confd:
+        (forward,backward) = parse_attribute_map(confd["attribute_maps"])
+            
     ed = {
         "name": "http://%s/saml/test" % os.uname()[1],
         "valid_until": in_a_while(hours=96),
@@ -87,13 +121,13 @@ def entity_descriptor(confd):
     if "sp" in confd["service"]:
         # The SP
         ed["sp_sso_descriptor"] = do_sp_sso_descriptor(confd["service"]["sp"],
-                                    mycert)
+                                    mycert, backward)
     if "idp" in confd["service"]:
         ed["idp_sso_descriptor"] = do_idp_sso_descriptor(
-                                            confd["service"]["idp"],mycert)
+                                            confd["service"]["idp"], mycert)
     if "aa" in confd["service"]:
         ed["attribute_authority_descriptor"] = do_aa_descriptor(
-                                            confd["service"]["aa"],mycert)
+                                            confd["service"]["aa"], mycert)
             
     return ed
 
