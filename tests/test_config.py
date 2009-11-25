1f4b9bb2ee5f6c613fc1c185118e89ea0dda4c0b
Roland Hedberg <roland.hedberg@adm.umu.se>
2009-11-25 17:27:51 +0100

    more tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from saml2.config import Config
+from saml2.metadata import MetaData
 
 sp1 = {
     "entityid" : "urn:mace:umu.se:saml:roland:sp",
@@ -30,9 +30,53 @@ sp1 = {
     }
 }
 
+sp2 = {
+    "entityid" : "urn:mace:umu.se:saml:roland:sp",
+    "service": {
+        "sp":{
+            "name" : "Rolands SP",
+            "url" : "http://localhost:8087/",
+            "required_attributes": ["surName", "givenName", "mail"],
+            "optional_attributes": ["title"],
+            "idp": {
+                "" : "https://example.com/saml2/idp/SSOService.php",
+            },
+        }
+    },
+    "xmlsec_binary" : "/opt/local/bin/xmlsec1",
+}
+
+def _eq(l1,l2):
+    return set(l1) == set(l2)
+
 def test_1():
     c = Config()
     c.load(sp1)
     
     print c
-    assert False
+    service = c["service"]
+    assert service.keys() == ["sp"]
+    sp = service["sp"] 
+    assert _eq(sp.keys(),["url","name","idp"])
+    md = c["metadata"]
+    assert isinstance(md, MetaData)
+
+    assert len(sp["idp"]) == 1
+    assert sp["idp"].keys() == ["urn:mace:example.com:saml:roland:idp"]
+    assert sp["idp"].values() == ["http://localhost:8088/sso/"]
+
+def test_2():
+    c = Config()
+    c.load(sp2)
+    
+    print c
+    service = c["service"]
+    assert service.keys() == ["sp"]
+    sp = service["sp"] 
+    assert _eq(sp.keys(),['url', 'idp', 'optional_attributes', 'name', 
+                            'required_attributes'])
+
+    assert len(sp["idp"]) == 1
+    assert sp["idp"].keys() == [""]
+    assert sp["idp"].values() == [
+                            "https://example.com/saml2/idp/SSOService.php"]
