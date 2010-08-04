4c6b60304c9beedf059fd526d32120594701c8cd
Roland Hedberg <roland.hedberg@adm.umu.se>
2010-08-04 15:05:11 +0200

    Extra info needed in the schema modules that are not to be found in the XML schemas but in the standards themself

---
+++ src/schema/defs_samlp.py
@@ -0,0 +1,36 @@
+STATUS_SUCCESS = 'urn:oasis:names:tc:SAML:2.0:status:Success'
+STATUS_REQUESTER = 'urn:oasis:names:tc:SAML:2.0:status:Requester'
+STATUS_RESPONDER = 'urn:oasis:names:tc:SAML:2.0:status:Responder'
+STATUS_VERSION_MISMATCH = 'urn:oasis:names:tc:SAML:2.0:status:VersionMismatch'
+
+STATUS_AUTHN_FAILED = 'urn:oasis:names:tc:SAML:2.0:status:AuthnFailed'
+STATUS_INVALID_ATTR_NAME_OR_VALUE = (
+    'urn:oasis:names:tc:SAML:2.0:status:InvalidAttrNameOrValue')
+STATUS_INVALID_NAMEID_POLICY = (
+    'urn:oasis:names:tc:SAML:2.0:status:InvalidNameIDPolicy')
+STATUS_NO_AUTHN_CONTEXT = 'urn:oasis:names:tc:SAML:2.0:status:NoAuthnContext'
+STATUS_NO_AVAILABLE_IDP = 'urn:oasis:names:tc:SAML:2.0:status:NoAvailableIDP'
+STATUS_NO_PASSIVE = 'urn:oasis:names:tc:SAML:2.0:status:NoPassive'
+STATUS_NO_SUPPORTED_IDP = 'urn:oasis:names:tc:SAML:2.0:status:NoSupportedIDP'
+STATUS_PARTIAL_LOGOUT = 'urn:oasis:names:tc:SAML:2.0:status:PartialLogout'
+STATUS_PROXY_COUNT_EXCEEDED = (
+    'urn:oasis:names:tc:SAML:2.0:status:ProxyCountExceeded')
+STATUS_REQUEST_DENIED = 'urn:oasis:names:tc:SAML:2.0:status:RequestDenied'
+STATUS_REQUEST_UNSUPPORTED = (
+    'urn:oasis:names:tc:SAML:2.0:status:RequestUnsupported')
+STATUS_REQUEST_VERSION_DEPRECATED = (
+    'urn:oasis:names:tc:SAML:2.0:status:RequestVersionDeprecated')
+STATUS_REQUEST_VERSION_TOO_HIGH = (
+    'urn:oasis:names:tc:SAML:2.0:status:RequestVersionTooHigh')
+STATUS_REQUEST_VERSION_TOO_LOW = (
+    'urn:oasis:names:tc:SAML:2.0:status:RequestVersionTooLow')
+STATUS_RESOURCE_NOT_RECOGNIZED = (
+    'urn:oasis:names:tc:SAML:2.0:status:ResourceNotRecognized')
+STATUS_TOO_MANY_RESPONSES = (
+    'urn:oasis:names:tc:SAML:2.0:status:TooManyResponses')
+STATUS_UNKNOWN_ATTR_PROFILE = (
+    'urn:oasis:names:tc:SAML:2.0:status:UnknownAttrProfile')
+STATUS_UNKNOWN_PRINCIPAL = (
+    'urn:oasis:names:tc:SAML:2.0:status:UnknownPrincipal')
+STATUS_UNSUPPORTED_BINDING = (
+    'urn:oasis:names:tc:SAML:2.0:status:UnsupportedBinding')
---
+++ src/schema/defs_xmldsig.py
@@ -0,0 +1,14 @@
+
+ENCODING_BASE64 = 'http://www.w3.org/2000/09/xmldsig#base64'
+DIGEST_SHA1 = 'http://www.w3.org/2000/09/xmldsig#sha1'
+ALG_EXC_C14N = 'http://www.w3.org/2001/10/xml-exc-c14n#'
+SIG_DSA_SHA1 = 'http://www.w3.org/2000/09/xmldsig#dsa-sha1'
+SIG_RSA_SHA1 = 'http://www.w3.org/2000/09/xmldsig#rsa-sha1'
+MAC_SHA1 = 'http://www.w3.org/2000/09/xmldsig#hmac-sha1'
+
+C14N = 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
+C14N_WITH_C = 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments'
+
+TRANSFORM_XSLT = 'http://www.w3.org/TR/1999/REC-xslt-19991116'
+TRANSFORM_XPATH = 'http://www.w3.org/TR/1999/REC-xpath-19991116'
+TRANSFORM_ENVELOPED = 'http://www.w3.org/2000/09/xmldsig#enveloped-signature'
