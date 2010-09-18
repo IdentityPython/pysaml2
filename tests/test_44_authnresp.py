1c2708a92c5024d838577e1e6dd5e94e6678736e
Roland Hedberg <roland.hedberg@adm.umu.se>
2010-09-18 20:24:41 +0200

    Test of adding info about the authentication

--- tests/test_44_authnresp.py
+++ tests/test_44_authnresp.py
@@ -36,6 +36,15 @@ class TestAuthnResponse:
                     sign=True
                 )
 
+        self._resp_authn = server.do_response(
+                    "http://lingon.catalogix.se:8087/",   # consumer_url
+                    "12",                       # in_response_to
+                    "urn:mace:example.com:saml:roland:sp", # sp_entity_id
+                    {"eduPersonEntitlement":"Jeter"},
+                    name_id = name_id,
+                    authn=(saml.AUTHN_PASSWORD, "http://www.example.com/login")
+                )
+
         conf = config.Config()
         try:
             conf.load_file("tests/server.config")
@@ -90,3 +99,19 @@ class TestAuthnResponse:
         assert self.ar.session_id() == ID
         assert self.ar.name_id
 
+    def test_verify_w_authn(self):
+        xml_response = ("%s" % (self._resp_authn,)).split("\n",1)[1]
+        self.ar.outstanding_queries = {"12": "http://localhost:8088/sso"}
+        self.ar.requestor = "urn:mace:example.com:saml:roland:sp"
+        self.ar.timeslack = 10000
+        self.ar.loads(xml_response, decode=False)
+        self.ar.verify()
+
+        print self.ar.assertion
+        assert len(self.ar.assertion.authn_statement) == 1
+        authn_info = self.ar.authn_info()
+        assert len(authn_info) == 1
+        assert authn_info[0][0] == saml.AUTHN_PASSWORD
+        assert authn_info[0][1] == ["http://www.example.com/login"]
+        session_info = self.ar.session_info()
+        assert session_info["authn_info"] == authn_info
