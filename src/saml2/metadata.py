a1ac1ca9debb50ec8108c2697ca4734cb8636bb9
Roland Hedberg <roland.hedberg@adm.umu.se>
2011-03-14 17:00:05 +0100

    editorial

--- src/saml2/metadata.py
+++ src/saml2/metadata.py
@@ -628,13 +628,12 @@ def _localized_name(val, klass):
 def do_organization_info(conf):
     """ decription of an organization in the configuration is
     a dictionary of keys and values, where the values might be tuples:
-    
+
         "organization": {
             "name": ("AB Exempel", "se"),
             "display_name": ("AB Exempel", "se"),
             "url": "http://www.example.org"
         }
-    
     """
     try:
         corg = conf["organization"]
