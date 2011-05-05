659efcde54da74c5a6aa52e6fda1808863d21077
Roland Hedberg <roland.hedberg@adm.umu.se>
2011-05-05 11:43:04 +0200

    Added useful method when dealing with extensions

--- src/saml2/__init__.py
+++ src/saml2/__init__.py
@@ -375,7 +375,21 @@ class ExtensionContainer(object):
                 results.append(element)
 
         return results
-    
+
+    def extensions_as_elements(self, tag, schema):
+        """ Return extensions that has the given tag and belongs to the
+        given schema as native elements of that schema.
+
+        :param tag: The tag of the element
+        :param schema: Which schema the element should originate from
+        :return: a list of native elements
+        """
+        result = []
+        for ext in self.find_extensions(tag, schema.NAMESPACE):
+            ets = schema.ELEMENT_FROM_STRING[tag]
+            result.append(ets(ext.to_string()))
+        return result
+
 def make_vals(val, klass, klass_inst=None, prop=None, part=False,
                 base64encode=False):
     """
