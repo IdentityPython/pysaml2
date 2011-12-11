1d15ad24f749c8c1b325ff4f34b7714499abd311
Roland Hedberg <roland.hedberg@adm.umu.se>
2011-12-11 09:02:23 +0100

    Removed the dependencies on absolute values

--- tests/test_40_sigver.py
+++ tests/test_40_sigver.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 
-import os
 import base64
 
-from saml2 import sigver, make_instance
+from saml2 import sigver
 from saml2 import class_name
 from saml2 import time_util
 from saml2 import saml, samlp
 from saml2.s_utils import factory, do_attribute_statement
 from saml2.sigver import xmlsec_version, get_xmlsec_binary
 
-import xmldsig as ds
 from py.test import raises
 
 SIGNED = "saml_signed.xml"
@@ -23,31 +21,6 @@ PRIV_KEY = "test.key"
 
 def _eq(l1,l2):
     return set(l1) == set(l2)
-
-SIGNATURE_DIGEST = {
-    "1.2.14": [(
-        "kMuyOK17nyp4CbA1v7KE32rX4+NQQ8EvdglTK61uIMEo3ax0PgFU7bgZGey+Aj8H\nhTPVyAzWmBDxHpSCFe050PTtNoKHx7nXprLfhuQXsPq8s0KBoZR+2qYfVCkWYVX7\nT3zG/Tn+fesBA1zLo4lYdAovol7C35KAsAWoknmZdOE=",
-        "SXw3kqTf+PtTiUnI8nQ6xmrM3qw="),
-        (
-        "upeKPE1pkzXLy9BvKFOSTnjn4du59lQQ74TN5CqDGae9D21uY/zLuOWql7LiSTSi\nC945F0WrOvG7s0eZnpuNPZobdfdeCOffCMMrq5RQ2+abPFBamkjmceuEKGdO5PWQ\nt7B1GkzXAMMgeMuU+YmvIJkHbbv5Yn6M0/ICE/COaKQ=",
-        "uX92C/YDroqITDfDY1IeekGtZac="),
-        (
-        "xHECLk1jj4NBvk1jhGrb2mwnrLFKXk6JN3NogjMVMtnnarg9vtk7jYzy1M9RPWdj\nRSa2Jph7yVZJENm4bGuBkT91w+FYm2X4jREULPUsnupPHTQyhJEVZ07EhnluOWa3\n95KkqnZ5gbnTxn1ZvpsANzThLmYY3eSGzNXz+S7758M=",
-        "l36wHa6Lyed9ZeAZ3jFL77wPVQ4="
-        )
-    ],
-    "1.2.16":[
-        (
-        "imvo3quPyMND8yCv8D3LNCbeiG98hKl+F5VekEY5N7EEBoq7S3A7mArz4yZUVJVw\n1migufgOZEiZX80vzR0lwfjAEjwRp+NjKRvOcWHfIgjz+dG8q9n4LcI5YmsjveLa\n+iNTujev1PYA+UWf57S5mqGFoi0KaS8Xnp0FG1olAZ0=",
-        "0+0Td5mWbs+CF7xZeYSlcQ/pjKw="),
-        (
-        "NEoJEpCLRi35e+cK8fwInrThausuD3xNlKZFhZda6qS8GU93s8J3sKLpd5BwB9my\nesHX38c9WhQkXeuQu6O75hMwLWb7496vG+QcodaWvLJ8u/Cgp2XdQopkNWLOqLJC\n7XyLa0fEDhPY/kvX88kx9xBnA/VhIYVjQtNrTD9M5Q8=",
-        "gqe292uV8r7LfSomiMh9VS9wYZw="),
-        (
-        "DS5V623NrKCXmBjzCgVDUkPXSg8kMezZIeEqg8RC6Q/0/vjoBgZDt1hMvOmOX4Vf\nA1ckqeEEHnsqegjBRUUiV41SALJmKSVvUG5V29ZonGK4EXtdC5dxRPa/2tqN1i8N\nwtTlD7DE/YLAPIM5nhL8qHKKovQvwypZmC2YVmKIuQ0=",
-        "h6o97FThq9XqEzw+njeKjH45QgM="),
-    ]
-}
 
 CERT1 = """MIICsDCCAhmgAwIBAgIJAJrzqSSwmDY9MA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNV
 BAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJbnRlcm5ldCBX
@@ -106,14 +79,8 @@ def test_cert_from_instance_ssp():
 class TestSecurity():
     def setup_class(self):
         xmlexec = get_xmlsec_binary()
-        self.sec = sigver.SecurityContext(xmlexec, PRIV_KEY, "pem",
-                                            PUB_KEY, "pem", debug=1)
-
-        self.sign_digest = SIGNATURE_DIGEST["1.2.16"]
-#        try:
-#            self.sign_digest = SIGNATURE_DIGEST[xmlsec_version(xmlexec)]
-#        except KeyError:
-#            self.sign_digest = SIGNATURE_DIGEST["1.2.14"]
+        self.sec = sigver.SecurityContext(xmlexec, key_file=PRIV_KEY,
+                                          cert_file=PUB_KEY, debug=1)
 
         self._assertion = factory( saml.Assertion,
             version="2.0",
@@ -145,7 +112,8 @@ class TestSecurity():
     def test_sign_assertion(self):
         ass = self._assertion
         print ass
-        sign_ass = self.sec.sign_assertion_using_xmlsec("%s" % ass, nodeid=ass.id)
+        sign_ass = self.sec.sign_assertion_using_xmlsec("%s" % ass,
+                                                        nodeid=ass.id)
         #print sign_ass
         sass = saml.assertion_from_string(sign_ass)
         #print sass
@@ -154,15 +122,13 @@ class TestSecurity():
         assert sass.version == "2.0"
         assert sass.id == "11111"
         assert time_util.str_to_time(sass.issue_instant)
-        sig = sass.signature
+
         print xmlsec_version(get_xmlsec_binary())
-        print sig.signature_value.text
-        print self.sign_digest[0][0]
-        assert sig.signature_value.text == self.sign_digest[0][0]
-        assert len(sig.signed_info.reference) == 1
-        assert sig.signed_info.reference[0].digest_value
-        assert sig.signed_info.reference[0].digest_value.text == self.sign_digest[0][1]
-        
+
+        item = self.sec.check_signature(sass, node_name=class_name(sass))
+
+        assert isinstance(item, saml.Assertion)
+
     def test_sign_response(self):
         response = factory(samlp.Response,
                 assertion=self._assertion,
@@ -173,7 +139,7 @@ class TestSecurity():
                     (class_name(response), response.id)]
         s_response = sigver.signed_instance_factory( response, self.sec, to_sign)
             
-        assert s_response != None
+        assert s_response is not None
         print s_response
         print
         sass = s_response.assertion[0]
@@ -183,14 +149,11 @@ class TestSecurity():
                                 'version', 'signature', 'id'])
         assert sass.version == "2.0"
         assert sass.id == "11111"
-        assert time_util.str_to_time(sass.issue_instant)
-        sig = sass.signature
-        assert sig.signature_value.text == self.sign_digest[1][0]
-        
-        assert len(sig.signed_info.reference) == 1
-        assert sig.signed_info.reference[0].digest_value
-        digest = sig.signed_info.reference[0].digest_value.text
-        assert digest == self.sign_digest[1][1]
+
+        item = self.sec.check_signature(s_response,
+                                        node_name=class_name(s_response))
+        assert isinstance(item, samlp.Response)
+        assert item.id == "22222"
 
     def test_sign_response_2(self):
         assertion2 = factory( saml.Assertion,
@@ -206,32 +169,25 @@ class TestSecurity():
         response = factory(samlp.Response,
                 assertion=assertion2,
                 id="22233",
-                signature=sigver.pre_signature_part("22233"))
+                signature=sigver.pre_signature_part("22233", self.sec.my_cert))
 
         to_sign = [(class_name(assertion2), assertion2.id),
                     (class_name(response), response.id)]
 
-        s_response = sigver.signed_instance_factory( response, self.sec, to_sign)
+        s_response = sigver.signed_instance_factory(response, self.sec, to_sign)
 
-        assert s_response != None
-        print s_response
-        print
+        assert s_response is not None
         sass = s_response.assertion[0]
-        
-        print sass
-        assert _eq(sass.keyswv(), ['attribute_statement', 'issue_instant', 
+        assert _eq(sass.keyswv(), ['attribute_statement', 'issue_instant',
                                 'version', 'signature', 'id'])
         assert sass.version == "2.0"
         assert sass.id == "11122"
-        assert time_util.str_to_time(sass.issue_instant)
-        sig = sass.signature
-        assert sig.signature_value.text == self.sign_digest[2][0]
-        
-        assert len(sig.signed_info.reference) == 1
-        assert sig.signed_info.reference[0].digest_value
-        digest = sig.signed_info.reference[0].digest_value.text
-        assert digest == self.sign_digest[2][1]
 
+        item = self.sec.check_signature(s_response,
+                                        node_name=class_name(s_response))
+
+        assert isinstance(item, samlp.Response)
+        
     def test_sign_verify(self):        
         response = factory(samlp.Response,
                 assertion=self._assertion,
