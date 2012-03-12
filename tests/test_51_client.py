7420fae7b587891a1e52aa457c919b9a3ada67c0
root <root@pysaml2.ticotico.yaco.es>
2012-03-12 17:28:05 +0100

    Fix a syntax error in a test

--- tests/test_51_client.py
+++ tests/test_51_client.py
@@ -133,7 +133,7 @@ class TestClient:
                 if getattr(attribute,"friendly_name"):
                     assert False
                 seen.append("email")
-        assert set(seen) == {"givenName", "surname", "email"}
+        assert set(seen) == set(["givenName", "surname", "email"])
         
     def test_create_attribute_query_3(self):
         req = self.client.create_attribute_query("id1",
