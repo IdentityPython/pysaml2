93a9bf3a6a68cd15d4e40d45612a03a738ad8c8d
Fredrik Thulin <fredrik@thulin.net>
2013-06-11 13:45:11 +0200

    Improve MongoDB support.
    * Support configuration with MongoDB connection URI strings.
    * Handle connecting to replica sets.
    * Use standard notation database+collection rather than
      collection+sub_collection.
    
    Note that these changes are NOT expected to be fully backwards
    compatible for someone using mongodb with pysaml2 already.

--- src/saml2/mongo_store.py
+++ src/saml2/mongo_store.py
@@ -2,6 +2,8 @@ from hashlib import sha1
 import logging
 
 from pymongo import MongoClient
+from pymongo.mongo_replica_set_client import MongoReplicaSetClient
+import pymongo.uri_parser
 from saml2.eptid import Eptid
 from saml2.mdstore import MetaData
 from saml2.s_utils import PolicyError
@@ -49,10 +51,9 @@ def context_match(cfilter, cntx):
 class SessionStorageMDB(object):
     """ Session information is stored in a MongoDB database"""
 
-    def __init__(self, collection=""):
-        connection = MongoClient()
-        db = connection[collection]
-        self.assertion = db.assertion
+    def __init__(self, database="", collection="assertion", **kwargs):
+        db = _mdb_get_database(database, **kwargs)
+        self.assertion = db[collection]
 
     def store_assertion(self, assertion, to_sign):
         name_id = assertion.subject.name_id
@@ -129,9 +130,9 @@ class SessionStorageMDB(object):
 
 
 class IdentMDB(IdentDB):
-    def __init__(self, collection="", domain="", name_qualifier=""):
+    def __init__(self, database="", collection="ident", domain="", name_qualifier=""):
         IdentDB.__init__(self, None, domain, name_qualifier)
-        self.mdb = MDB(collection, "ident")
+        self.mdb = MDB(database=database, collection=collection)
         self.mdb.primary_key = "user_id"
 
     def in_store(self, _id):
@@ -195,10 +196,9 @@ class IdentMDB(IdentDB):
 class MDB(object):
     primary_key = "mdb"
 
-    def __init__(self, collection="", sub_collection=""):
-        connection = MongoClient()
-        _db = connection[collection]
-        self.db = _db[sub_collection]
+    def __init__(self, database, collection, **kwargs):
+        _db = _mdb_get_database(database, **kwargs)
+        self.db = _db[collection]
 
     def store(self, value, **kwargs):
         if value:
@@ -250,11 +250,52 @@ class MDB(object):
         self.db.drop()
 
 
+
+def _mdb_get_database(database, **kwargs):
+    """
+    Helper-function to connect to MongoDB and return a database object.
+
+    Performs explicit authentication if a username is provided in a connection
+    string URI, since PyMongo does not always seem to do that as promised.
+
+    If database is *not* a connection to mongodb://localhost:27017 will be
+    made.
+
+    :params database: name as string or (uri, name)
+    :returns: pymongo database object
+    """
+    _conn = None
+    if isinstance(database, tuple):
+        uri, collection, = database
+        if uri.startswith("mongodb://"):
+            if "replicaSet=" in uri:
+                connection_factory = MongoReplicaSetClient
+            else:
+                connection_factory = MongoClient
+
+            if not 'tz_aware' in kwargs:
+                # default, but not forced
+                kwargs['tz_aware'] = True
+
+            _conn = connection_factory(uri, **kwargs)
+    if not _conn:
+        _conn = MongoClient()
+
+    _parsed_uri = pymongo.uri_parser.parse_uri(uri)
+    _db = _conn[_parsed_uri.get("database")]
+    if _parsed_uri.get("username", None):
+        _db.authenticate(
+            _parsed_uri.get("username", None),
+            _parsed_uri.get("password", None)
+            )
+
+    return _db
+
 #------------------------------------------------------------------------------
 class EptidMDB(Eptid):
-    def __init__(self, secret, collection="", sub_collection="eptid"):
+    def __init__(self, secret, database="", collection="eptid"):
         Eptid.__init__(self, secret)
-        self.mdb = MDB(collection, sub_collection)
+        self.mdb = MDB(database, collection)
         self.mdb.primary_key = "eptid_key"
 
     def __getitem__(self, key):
@@ -330,9 +371,9 @@ def export_mdstore_to_mongo_db(mds, collection, sub_collection=""):
 
 
 class MetadataMDB(MetaData):
-    def __init__(self, onts, attrc, collection="", sub_collection=""):
+    def __init__(self, onts, attrc, database="", collection=""):
         MetaData.__init__(self, onts, attrc)
-        self.mdb = MDB(collection, sub_collection)
+        self.mdb = MDB(database, collection)
         self.mdb.primary_key = "entity_id"
 
     def _ext_service(self, entity_id, typ, service, binding):
