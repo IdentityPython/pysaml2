5b9a12b227abee89246e078e1c6799294eb7ee3e
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-06-14 18:49:11 +0200

    InCommon entity categories added

---
+++ src/saml2/entity_category/incommon.py
@@ -0,0 +1,10 @@
+__author__ = 'rolandh'
+
+INC = "http://id.incommon.org/category/research-and-scholarship"
+
+RELEASE = {
+    "": ["eduPersonTargetedID"],
+    INC: ["eduPersonPrincipalName", "eduPersonScopedAffiliation", "mail",
+          "givenName", "sn", "displayName"]
+}
+
