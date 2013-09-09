e3ea651c717d3d2c487fc36d2a207620695fdffe
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-09-09 08:29:17 +0200

    Updated the Austrian government entity categories.

--- src/saml2/entity_category/at_egov_pvp2.py
+++ src/saml2/entity_category/at_egov_pvp2.py
@@ -1,25 +1,37 @@
-__author__ = 'rainerh'  #2013-08-28
-# Preliminary version, not including all attributes defined for eGov Token
+__author__ = 'rhoerbe'  #2013-09-05
+# Entity Categories specifying the PVP eGov Token as of "PVP2-Allgemein V2.1.0", http://www.ref.gv.at/
 
 
-PRINCIPAL = ["PVP-PRINCIPALNAME", 
-             "PVP-GIVENNAME", 
-]
-ORG_INFO  = ["PVP-OU", 
-             "PVP-OU-GV-OU-ID", 
-]
-OTHER = ["PVP-GID",
-         "PVP-ROLES",
-         "PVP-BPK",
-         "PVP-USERID",
-         "PVP-MAIL",
-         "PVP-TEL",
+EGOVTOKEN = ["PVP-VERSION",
+             "PVP-PRINCIPALNAME",
+             "PVP-GIVENNAME",
+             "PVP-BIRTHDATE",
+             "PVP-USERID",
+             "PVP-GID",
+             "PVP-BPK",
+             "PVP-MAIL",
+             "PVP-TEL",
+             "PVP-PARTICIPANT-ID",
+             "PVP-PARTICIPANT-OKZ",
+             "PVP-OU-OKZ",
+             "PVP-OU",
+             "PVP-OU-GV-OU-ID",
+             "PVP-FUNCTION",
+             "PVP-ROLES",
 ]
 
-# These give you access to information
+
+CHARGEATTR = ["PVP-INVOICE-RECPT-ID",
+              "PVP-COST-CENTER-ID",
+              "PVP-CHARGE-CODE",
+]
+
+# all eGov Token attributes except (1) transaction charging and (2) chaining
 PVP2 = "http://www.ref.gv.at/ns/names/agiz/pvp/egovtoken"
+# transaction charging extension
 PVP2CHARGE = "http://www.ref.gv.at/ns/names/agiz/pvp/egovtoken-charge"
 
 RELEASE = {
-    PVP2: PRINCIPAL + ORG_INFO + OTHER,
+    PVP2: EGOVTOKEN,
+    PVP2CHARGE: CHARGEATTR,
 }
