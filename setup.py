e9832b7b3bb028170562cacde3dbb52f13adba85
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-12-16 10:12:41 +0100

    Set version number

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='pysaml2',
-    version='1.1.0',
+    version='1.2.0beta',
     description='Python implementation of SAML Version 2 to be used in a WSGI environment',
     # long_description = read("README"),
     author='Roland Hedberg',
