ff6135955d05551762a0123e2b3df25512a043bb
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-02-15 10:35:10 +0100

    Modified logging setup.

--- src/idp_test/__init__.py
+++ src/idp_test/__init__.py
@@ -40,8 +40,6 @@ SCHEMA = [dri, idpdisc, md, mdattr, mdui, saml, ui, xmldsig, xmlenc]
 
 __author__ = 'rolandh'
 
-logger = logging.getLogger("")
-logger.setLevel(logging.DEBUG)
 #formatter = logging.Formatter("%(asctime)s %(name)s:%(levelname)s %(message)s")
 formatter_2 = logging.Formatter("%(delta).6f - %(levelname)s - [%(name)s] %(message)s")
 
@@ -54,10 +52,11 @@ streamhandler.setFormatter(formatter_2)
 memoryhandler = logging.handlers.MemoryHandler(1024*10, logging.DEBUG)
 memoryhandler.addFilter(cf)
 
-saml2testlog = logging.getLogger("saml2test")
-saml2testlog.addHandler(memoryhandler)
-saml2testlog.setLevel(logging.DEBUG)
-
+logger = logging.getLogger("saml2test")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(memoryhandler)
+#logger.addHandler(streamhandler)
+logger.setLevel(logging.DEBUG)
 
 def recursive_find_module(name, path=None):
     parts = name.split(".")
