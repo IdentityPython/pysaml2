817733a82140fcc6ec77e600514090d49eba6e12
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-04-15 00:11:21 +0200

    Added tool to keep attributemaps in sync. Ran it on the standard maps.

--- src/saml2/attributemaps/adfs_v1x.py
+++ src/saml2/attributemaps/adfs_v1x.py
@@ -1,20 +1,18 @@
-# See http://technet.microsoft.com/en-us/library/cc733065(v=ws.10).aspx
-# and http://technet.microsoft.com/en-us/library/ee913589(v=ws.10).aspx
-# for information regarding the default claim types supported by
-# Microsoft ADFS v1.x.
+CLAIMS = 'http://schemas.xmlsoap.org/claims/'
+
 
 MAP = {
     "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified",
-    "fro": {
-        'http://schemas.xmlsoap.org/claims/commonname': 'commonName',
-        'http://schemas.xmlsoap.org/claims/emailaddress': 'emailAddress',
-        'http://schemas.xmlsoap.org/claims/group': 'group',
-        'http://schemas.xmlsoap.org/claims/upn': 'upn',
-        },
-    "to": {
-        'commonName': 'http://schemas.xmlsoap.org/claims/commonname',
-        'emailAddress': 'http://schemas.xmlsoap.org/claims/emailaddress',
-        'group': 'http://schemas.xmlsoap.org/claims/group',
-        'upn': 'http://schemas.xmlsoap.org/claims/upn',
+    'fro': {
+        CLAIMS+'commonname': 'commonName',
+        CLAIMS+'emailaddress': 'emailAddress',
+        CLAIMS+'group': 'group',
+        CLAIMS+'upn': 'upn',
+    },
+    'to': {
+        'commonName': CLAIMS+'commonname',
+        'emailAddress': CLAIMS+'emailaddress',
+        'group': CLAIMS+'group',
+        'upn': CLAIMS+'upn',
     }
 }
