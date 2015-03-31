d55bfeebe70102278181de9d6bca8b44deb9a1aa
Shabda Raaj <shabda@agiliq.com>
2015-03-31 16:12:30 +0530

    RTD doesn't have alabaster theme so use the RTD theme
    https://github.com/rtfd/readthedocs.org/issues/1202

--- doc/conf.py
+++ doc/conf.py
@@ -92,6 +92,11 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 html_theme = 'alabaster'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if on_rtd:  # only import and set the theme if we're building docs locally
+    html_theme = 'sphinx_rtd_theme'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
