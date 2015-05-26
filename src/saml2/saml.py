23a388788a749f82e3123025130845c4d54d6213
Clint Byrum <clint@fewbar.com>
2015-05-26 14:40:33 -0700

    Correct python3-incompatible assumptions
    The s_utils tests reveal some incompatibilities with python2-specific
    behaviors being assumed.

--- src/saml2/saml.py
+++ src/saml2/saml.py
@@ -193,6 +193,8 @@ class AttributeValueBase(SamlBase):
             val = base64.encodestring(val)
             self.set_type("xs:base64Binary")
         else:
+            if isinstance(val, six.binary_type):
+                val = val.decode('utf-8')
             if isinstance(val, six.string_types):
                 if not typ:
                     self.set_type("xs:string")
