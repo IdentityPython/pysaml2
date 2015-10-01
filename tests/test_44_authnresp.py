57f7569bbf8d90e523918803134da8f2329cce23
Hans Hörberg <hans.horberg@umu.se>
2015-10-01 13:51:03 +0200

    Added the timestamp when the authentication took place to the authn_info method.
    Updated the test to verify the change.

--- tests/test_44_authnresp.py
+++ tests/test_44_authnresp.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from contextlib import closing
+from datetime import datetime
+from dateutil import parser
 from saml2.authn_context import INTERNETPROTOCOLPASSWORD
 
 from saml2.server import Server
@@ -123,6 +125,9 @@ class TestAuthnResponse:
         assert len(authn_info) == 1
         assert authn_info[0][0] == INTERNETPROTOCOLPASSWORD
         assert authn_info[0][1] == ["http://www.example.com/login"]
+        now = datetime.now()
+        dt = parser.parse(authn_info[0][2])
+        assert now.year == dt.year and now.month == dt.month and now.day == dt.day
         session_info = self.ar.session_info()
         assert session_info["authn_info"] == authn_info
 
