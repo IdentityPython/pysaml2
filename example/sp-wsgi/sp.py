20feb780c2b8b7b2145ee85117e12ebf0c3bfb28
Willem D'Haeseleer <dhwillem@gmail.com>
2015-10-13 20:26:13 -0700

    Add fallback for SimpleCookie import

--- example/sp-wsgi/sp.py
+++ example/sp-wsgi/sp.py
@@ -4,7 +4,10 @@ import logging
 import re
 import argparse
 import os
-from future.backports.http.cookies import SimpleCookie
+try:
+    from future.backports.http.cookies import SimpleCookie
+except:
+    from Cookie import SimpleCookie
 import six
 
 from saml2.extension.pefim import SPCertEnc
