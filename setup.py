1eb06f7374cc55c1fd99d6349f72be0644b30b02
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-11-12 10:54:53 +0100

    Update tox.ini to actually run tests and make Travis use it. Remove py.test runner from setup.py.

--- setup.py
+++ setup.py
@@ -6,21 +6,6 @@ import sys
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-
-class PyTest(TestCommand):
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
-
 install_requires = [
     # core dependencies
     'decorator',
@@ -85,6 +70,4 @@ setup(
     },
     install_requires=install_requires,
     zip_safe=False,
-    test_suite='tests',
-    cmdclass={'test': PyTest},
 )
