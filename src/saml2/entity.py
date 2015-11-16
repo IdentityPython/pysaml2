576317ae801d523700b2a2074274340d7a1c204b
Roland Hedberg <roland.hedberg@adm.umu.se>
2015-11-16 21:56:41 +0100

    form_post are suppost to use POST not GET.

--- src/saml2/entity.py
+++ src/saml2/entity.py
@@ -224,7 +224,7 @@ class Entity(HTTPBase):
             info = self.use_http_form_post(msg_str, destination,
                                            relay_state, typ)
             info["url"] = destination
-            info["method"] = "GET"
+            info["method"] = "POST"
         elif binding == BINDING_HTTP_REDIRECT:
             logger.info("HTTP REDIRECT")
             info = self.use_http_get(msg_str, destination, relay_state, typ,
