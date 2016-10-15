1d232bc34c319f2fb0bbe479efffc5f48bbb3198
Joe Nottingham <joseph.nottingham@gmail.com>
2016-10-15 15:22:42 -0400

    travis build config

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ with open('src/saml2/__init__.py', 'r') as fd:
                         fd.read(), re.MULTILINE).group(1)
 
 setup(
-    name='pysaml2',
+    name='pytesttest',
     version=version,
     description='Python implementation of SAML Version 2',
     # long_description = read("README"),
-    author='Roland Hedberg',
-    author_email='roland.hedberg@adm.umu.se',
+    author='test',
+    author_email='test@example.com',
     license='Apache 2.0',
     url='https://github.com/rohe/pysaml2',
 
