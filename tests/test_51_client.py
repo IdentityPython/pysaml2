1979ae7ab081107898f0817dc91baea7e21a7281
Roland Hedberg <roland@catalogix.se>
2017-10-11 15:38:13 +0200

    Removed unused import

--- tests/test_51_client.py
+++ tests/test_51_client.py
@@ -35,7 +35,7 @@ from saml2.saml import NAMEID_FORMAT_TRANSIENT
 from saml2.saml import NameID
 from saml2.samlp import SessionIndex
 from saml2.server import Server
-from saml2.sigver import pre_encryption_part, make_temp, pre_encrypt_assertion
+from saml2.sigver import pre_encryption_part, pre_encrypt_assertion
 from saml2.sigver import rm_xmltag
 from saml2.sigver import verify_redirect_signature
 from saml2.s_utils import do_attribute_statement
