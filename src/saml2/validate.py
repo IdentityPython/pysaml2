56e7a9259124bff0eb09057668c87fa4fb1aa24a
Loïc Padier <loic@clustree.com>
2017-12-07 17:26:17 +0100

    Fix typo in validate_on_or_after error message

--- src/saml2/validate.py
+++ src/saml2/validate.py
@@ -91,9 +91,9 @@ def validate_on_or_after(not_on_or_after, slack):
         now = time_util.utc_now()
         nooa = calendar.timegm(time_util.str_to_time(not_on_or_after))
         if now > nooa + slack:
-            now_str=time.strftime('%Y-%M-%dT%H:%M:%SZ', time.gmtime(now))
+            now_str=time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(now))
             raise ResponseLifetimeExceed(
-                "Can't use repsonse, too old (now=%s + slack=%d > " \
+                "Can't use response, too old (now=%s + slack=%d > " \
                 "not_on_or_after=%s" % (now_str, slack, not_on_or_after))
         return nooa
     else:
