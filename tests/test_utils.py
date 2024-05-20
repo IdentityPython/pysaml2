b4fd5bc33ed002495ae393a35799c845eb39de33
AndrewFoxUUID <andrewisaacfox@gmail.com>
2024-05-20 09:41:22 -0700

    test_utils fix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,16 +5,19 @@ ELIDED_TAGS = [
     "CipherValue"
 ]
 
-def tag_name(tag: str) -> str:
-    if tag.startswith("..."):
-        tag = tag[3:]
+def tag_name(tag: str, include_namespace: bool = True) -> str:
     name = ""
-    for char in tag:
-        if char in "</":
-            continue
+    start = 0
+    if tag[start] == "<":
+        start += 1
+        if tag[start] == "/":
+            start += 1
+    for char in tag[start:]:
         if char in " \n/>":
             break
         name += char
+    if not include_namespace:
+        name = name.split(":")[-1]
     return name
 
 def pretty_print_xml(xml: str):
@@ -46,18 +49,17 @@ def pretty_print_xml(xml: str):
             closed = True
             continue
 
-    first = True
     space_count = 0
     prev = ""
     for tag in tags:
         istag = tag.startswith("<")
         closing = istag and tag[1] == "/"
-        if istag and prev.startswith("<") and not first:
-            print()
-        if closing: space_count -= 4
-        if istag and prev.startswith("<"): print(" " * space_count, end="")
-        print(tag if istag or tag_name(prev).split(":")[-1] not in ELIDED_TAGS else "...", end="")
-        if istag and tag_groups[tag_name(tag)] and not closing: space_count += 4
+        if closing and tag_groups[tag_name(tag)]:
+            space_count -= 4
+        if istag and prev.startswith("<"): # istag and prev_istag
+            print("\n" + (" " * space_count), end="")
+        print(tag if (istag or tag_name(prev).split(":")[-1] not in ELIDED_TAGS) else "...", end="")
+        if istag and tag_groups[tag_name(tag)] and not closing:
+            space_count += 4
         prev = tag
-        first = False
     print()
